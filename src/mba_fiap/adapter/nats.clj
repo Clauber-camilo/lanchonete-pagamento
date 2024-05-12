1496034e6ab8e37ef8922656042e717121a24780
Clauber Camilo <clauber_camilo@live.com>
2024-05-12 18:44:48 -0300

    chore: remove comment from nats file

--- src/mba_fiap/adapter/nats.clj
+++ src/mba_fiap/adapter/nats.clj
@@ -79,27 +79,3 @@
 (defmethod ig/resolve-key ::nats
   [_ nats]
   nats)
-
-
-(comment
-
-  (with-open [c (nats-client {:url               "nats://66.51.121.86:4222"
-                              :app-name          "lanchonete"
-                              ; :subjects-handlers {"lanchonete.*" #(prn (.getSubject %) " " (String. (.getData %)) "----" (bean %) "\n" (str %))}})]
-                              :subjects-handlers {
-                                                  ; "lanchonete.novo-pedido" 
-                                                  ; #(prn (String. (.getData %)))
-                                                  }})]
-
-    (doseq [_ (range 1)]
-      (Thread/sleep 500)
-      (publish c "novo-pedido" 
-               (str {
-                    :id #uuid "2985094e-43ea-4105-8e4e-239913f72d33" 
-                    :id-cliente #uuid "01c1e2be-3ce6-4ff6-9a88-6c75124840b0"
-                    :numero-do-pedido "fbb98663-77ab-4560-a065-6b9b833c190f"
-                    :produtos nil
-                    :status "recebido"
-                    :total 1238
-                    :created-at nil}))))
-  )
