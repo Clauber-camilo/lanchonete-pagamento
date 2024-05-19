910c9c84534cc11f0375aebd054d27ff93bc22ae
Clauber Camilo <clauber_camilo@live.com>
2024-05-18 21:45:33 -0300

    worlflow

--- build.clj
+++ build.clj
@@ -28,10 +28,11 @@
                     :main      'clojure.main
                     :main-args ["-m"
                                 "cloverage.coverage"
-                                "--junit"
+                                "--codecov"
                                 "--no-html"
                                 "--test-ns-path" "test"
-                                "--src-ns-path" "src"]})
+                                "--src-ns-path" "src"
+                                "--runner" "eftest"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
